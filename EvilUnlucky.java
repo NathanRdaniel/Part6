c7642ef96c99b2ca4490526f07ea3dd2506da063
NathanRdaniel <42808505+NathanRdaniel@users.noreply.github.com>
2018-11-07 09:03:35 -0700

    Update EvilUnlucky.java

--- EvilUnlucky.java
+++ EvilUnlucky.java
@@ -3,44 +3,44 @@ public class EvilUnlucky{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a nonnegative integer:");
-        long n= scan.nextLong();
-        long d = n;
-        long f = n;
-        long c = n;
-        long g = n;
+        long num= scan.nextLong();
+        long remander = num;
+        long original = num;
+        long work = num;
+        long g = num;
         int i = 1;
         int count = 0;
        while(i > 0) {
 
-        n = EvilUnlucky.divide(n);
+        num = EvilUnlucky.divide(n);
        
-       d = EvilUnlucky.remander(d);
-       if(d == 1){
+       remander = EvilUnlucky.remander(remander);
+       if(remander == 1){
         count ++;
     }
     if (count == 3){
        count = count - 2;
     }
-     d = n;
+     remander = num;
      
 
        if(n == 0){
            i = 0;
            if(count == 1 || count == 3){
-               System.out.println(f + " Is Odious");
+               System.out.println(original + " Is Odious");
            }
            else{
-            System.out.println(f + " Is evil");
+            System.out.println(original + " Is evil");
            }
        }
        }
        i = 1;
        while(i > 0) {
 
-        c = EvilUnlucky.divide(c);
-        if(c == 13){
+        work = EvilUnlucky.divide(c);
+        if(work == 13){
             i = 0;
-            System.out.println(f + " Is unlucky.");
+            System.out.println(original + " Is unlucky.");
         }
        g = EvilUnlucky.remander(g);
      g = c;
@@ -49,20 +49,20 @@ public class EvilUnlucky{
         System.out.println("Enter a nonnegative integer:");
         c= scan.nextLong();
         g= c;
-        f = c;
+        original = c;
        }
        }
     }
-    public  static long divide(long n){
+    public  static long divide(long num){
         
-        n = (n / 2);
+        num = (num / 2);
        // System.out.println(n);
     
-    return n;
+    return num;
     }
-    public  static long remander(long d){
-        d = (d % 2);
-        System.out.println(d);
-        return d;
+    public  static long remander(long remander){
+        remander = (remander % 2);
+        System.out.println(remander);
+        return remander;
         }
 }
